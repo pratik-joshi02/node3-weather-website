e7b1d8904c80e2ca495785b293197040c9611a0e
pratik-joshi02 <pratik2002j@gmail.com>
2022-01-20 12:50:05 +0530

    some changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,41 @@
-const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('#check')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const wet_desc = document.querySelector('#wet-desc')
+const wet_temp = document.querySelector('#wet-temp')
+const wet_max_temp = document.querySelector('#wet-max-temp')
+const wet_min_temp = document.querySelector('#wet-min-temp')
+const wet_feels_temp = document.querySelector('#wet-feels-temp')
+const wet_cloud = document.querySelector('#wet-cloud')
+const wet_humidity = document.querySelector('#wet-humidity')
+const wet_pressure = document.querySelector("#wet-pressure")
+const wet_wind = document.querySelector("#wet-wind")
+const wet_icon = document.querySelector("#wet-icon")
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm.addEventListener('click', (e) => {
     e.preventDefault()
 
     const location = search.value
 
     messageOne.textContent= 'Loading....'
-    messageTwo.textContent= ''
-
     fetch('/weather?address='+ encodeURIComponent(location)).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
-                messageTwo.textContent = data.forecast
-                messageOne.textContent = data.location
+                wet_desc.textContent = data.forecast.desc
+                wet_temp.textContent = data.forecast.curr_temp
+                wet_max_temp.textContent = data.forecast.max_temp
+                wet_min_temp.textContent = data.forecast.min_temp
+                wet_feels_temp.textContent = data.forecast.feels_temp
+                wet_cloud.textContent = data.forecast.cloud_cover
+                wet_humidity.textContent = data.forecast.humidity
+                wet_pressure.textContent = data.forecast.pressure
+                wet_wind.textContent = data.forecast.wind
+                messageOne.textContent = data.address
+                const val_icon = data.forecast.icon_id
+                wet_icon.innerHTML = `<img src="${val_icon}">`
+                console.log(data)
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,26 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url= 'http://api.weatherstack.com/current?access_key=8e1de044744420f833ebbd671770b38a&query='+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude)
-
+    //const url= 'http://api.weatherstack.com/current?access_key=8e1de044744420f833ebbd671770b38a&query='+ encodeURIComponent(latitude) +','+ encodeURIComponent(longitude)
+    const url= 'http://api.openweathermap.org/data/2.5/onecall?lat='+encodeURIComponent(latitude)+'&lon='+encodeURIComponent(longitude)+'&exlude=minutely,hourly&appid=72f67ab02ac5208ac07bd384db2051f2&units=metric'
     request({url, json: true}, (error, {body } = {}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         }else if (body.error) {
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + ' degrees out.')
+            callback(undefined, {
+                desc: body.current.weather[0].description ,
+                icon_id: "https://openweathermap.org/img/wn/"+body.current.weather[0].icon+"@4x.png" ,
+                curr_temp: Math.round(body.current.temp)+"\u00B0",  
+                feels_temp: Math.round(body.current.feels_like)+"\u00B0",  
+                max_temp: Math.round(body.daily[0].temp.max)+"\u00B0",  
+                min_temp: Math.round(body.daily[0].temp.min)+"\u00B0",  
+                wind: body.current.wind_speed+"km/h",
+                cloud_cover: body.current.clouds + "\u0025",
+                humidity: body.current.humidity + "\u0025",
+                pressure: body.current.pressure + "hPa"
+            })
         }
     })
 }
